6cb0f7fb7789b1b8020d98c34dbcee4f57d5bf71
Darío Rivera <fermius.us@gmail.com>
2025-12-31 13:22:28 -0500

    docs: add request access token example

---
+++ examples/oauth2/request_access_token.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once(__DIR__ . '/../../vendor/autoload.php');
+
+use EasyHttp\DiscordClient\Authorization;
+use EasyHttp\DiscordClient\DiscordClient;
+
+$clientId = 'your-client-id-here';
+$clientSecret = 'your-client-secret-here';
+
+$authorization = new Authorization();
+$authorization->setClientCredentials($clientId, $clientSecret);
+$client = new DiscordClient($authorization);
+
+$response = $client->requestAccessToken(['identify', 'email']);
+
+var_dump($response->getStatusCode(), $response->getBody());
